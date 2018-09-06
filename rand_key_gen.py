8972dbdf764c6a7b3232fd87910393daa1edc254
adamrocha <adamrocha13@gmail.com>
2018-09-06 05:04:08 -0700

    adding punct

--- rand_key_gen.py
+++ rand_key_gen.py
@@ -5,5 +5,5 @@ from random import randint
 import string
 
 characters = string.ascii_letters + string.punctuation + string.digits
-password = "".join(choice(characters) for x in range(randint(16, 24)))
+password = "".join(choice(characters) for x in range(randint(24, 32)))
 print(password)
