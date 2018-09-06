0c7af9c20a1f6fc2a630618cc197209372ead5e9
adamrocha <adamrocha13@gmail.com>
2018-09-06 04:35:48 -0700

    adding key solutions

---
+++ password.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from random import choice
+from random import randint
+import string
+
+characters = string.ascii_letters + string.punctuation + string.digits
+password = "".join(choice(characters) for x in range(randint(8, 16)))
+print(password)
