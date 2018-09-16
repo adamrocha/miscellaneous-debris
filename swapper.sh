2f9bb63987c243d492f018ce0b2dfef00399aa54
adamrocha <adamrocha13@gmail.com>
2018-09-16 08:59:29 -0700

    foo

--- swapper.sh
+++ swapper.sh
@@ -8,8 +8,8 @@
 
 SUM=0
 OVERALL=0
-for DIR in $(find /proc/ -maxdepth 1 -type d | grep -E "^/proc/[0-9]")
-#for DIR in $(find /proc/ -maxdepth 1 -type d -regex "^/proc/[0-9]+")
+#for DIR in $(find /proc/ -maxdepth 1 -type d | grep -E "^/proc/[0-9]")
+for DIR in $(find /proc/ -maxdepth 1 -type d -regex "^/proc/[0-9]+")
 do
     PID=$(echo "$DIR" | cut -d / -f 3)
     PROGNAME=$(ps -p "$PID" -o comm --no-headers)
