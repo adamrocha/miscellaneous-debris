1c4fe82b630234bb9198843a235824d8f780fd86
adamrocha <adamrocha13@gmail.com>
2018-09-15 21:38:34 -0700

    Clean up syntax

--- swapper.sh
+++ swapper.sh
@@ -2,16 +2,19 @@
 # Get current swap usage for all running processes
 SUM=0
 OVERALL=0
-for DIR in `find /proc/ -maxdepth 1 -type d | egrep "^/proc/[0-9]"`
+for DIR in $(find /proc/ -maxdepth 1 -type d | grep -E "^/proc/[0-9]")
 do
-    PID=`echo $DIR | cut -d / -f 3`
-    PROGNAME=`ps -p $PID -o comm --no-headers`
-    for SWAP in `grep Swap $DIR/smaps 2>/dev/null | awk '{ print $2 }'`
+    PID=$(echo "$DIR" | cut -d / -f 3)
+    PROGNAME=$(ps -p "$PID" -o comm --no-headers)
+    for SWAP in $(grep Swap "$DIR"/smaps 2>/dev/null | awk '{ print $2 }')
     do
-        let SUM=$SUM+$SWAP
+        (( SUM=SUM+SWAP ))
     done
+    if [ "$SUM" -gt 0 ]
+    then
     echo "PID=$PID - Swap used: $SUM - ($PROGNAME)"
-    let OVERALL=$OVERALL+$SUM
+  fi
+    (( OVERALL=OVERALL+SUM ))
     SUM=0
 done
 echo "Overall swap used: $OVERALL"
