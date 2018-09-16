c28788cff5c69fe259bdef524f11286be43e26ad
adamrocha <adamrocha13@gmail.com>
2018-09-15 22:45:11 -0700

    tweaks

--- swapper.sh
+++ swapper.sh
@@ -4,10 +4,12 @@
 SUM=0
 OVERALL=0
 for DIR in $(find /proc/ -maxdepth 1 -type d | grep -E "^/proc/[0-9]")
+#for DIR in $(find /proc/ -maxdepth 1 -type d -regex "^/proc/[0-9]+")
 do
     PID=$(echo "$DIR" | cut -d / -f 3)
     PROGNAME=$(ps -p "$PID" -o comm --no-headers)
     for SWAP in $(grep Swap "$DIR"/smaps 2>/dev/null | awk '{ print $2 }')
+#    for SWAP in $(grep VmSwap "$DIR"/status 2>/dev/null | awk '{ print $2 }')
     do
         (( SUM=SUM+SWAP ))
     done
