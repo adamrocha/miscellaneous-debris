c65b2e0ed0aae3de9867f45c1717cf32cb1c0bec
adamrocha <adamrocha13@gmail.com>
2018-09-17 11:57:32 -0700

    cleanup

--- backup_trigger.sh
+++ backup_trigger.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
 # Report failed backups over 24hrs
 
 currentTime=$(date +%s)
-backupFileCreated=$(stat -c %X $(ls -1rt /mnt/<backup_folder>/* |tail -1))
+backupFileCreated=$(stat -c %X "$(ls -1rt /mnt/nuzenoss_backups/* |tail -1)")
 difference=$((currentTime - backupFileCreated))
+sinceDate=$(date -d@"$backupFileCreated")
 
 if [ $difference -ge 86400 ] ; then
-  curl -k -u '<user>:<pass>' -X POST -H 'Content-Type:application/json' -d '{"action":"EventsRouter", "method":"add_event", "data":[{"summary":"Error: Backups have not been done since `date -d @$backupFileCreated`. Please generate a ticket for review during business hours.", "device":"backups", "component":"", "severity":"Error", "evclasskey":"", "evclass":"/Status"}], "type":"rpc", "tid":1}' 'https://<host>/zport/dmd/evconsole_router'
+  curl -k -u "operations:Operations123" -X POST -H "Content-Type:application/json" -d "{\"action\":\"EventsRouter\", \"method\":\"add_event\", \"data\":[{\"summary\":\"Error: Backups have not been completed since $sinceDate. Please generate a ticket for review during business hours.\", \"device\":\"Backups\", \"component\":\"\", \"severity\":\"Error\", \"evclasskey\":\"\", \"evclass\":\"/Status\"}], \"type\":\"rpc\", \"tid\":1}" "https://zenoss-staging.noa.com/zport/dmd/evconsole_router"
 fi
