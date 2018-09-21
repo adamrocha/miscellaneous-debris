0e0652e0fdf3ca7e2614c5d18aba286f9888cd09
adamrocha <adamrocha13@gmail.com>
2018-09-21 13:59:57 -0700

    cleanup

--- swapper.sh
+++ swapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Get current swap usage for all running processes
-# Pipe the output to "sort -nk3" to get sorted output
+# Pipe the output to "sort -rnk3" to get sorted output
 # Erik Ljungstrom 27/05/2011 (kernel 2.6.16)
 # Modified by Mikko Rantalainen 2012-08-09
 # Modified by Marc Methot 2014-09-18
@@ -8,13 +8,11 @@
 
 SUM=0
 OVERALL=0
-#for DIR in $(find /proc/ -maxdepth 1 -type d | grep -E "^/proc/[0-9]")
 for DIR in $(find /proc/ -maxdepth 1 -type d -regex "^/proc/[0-9]+")
 do
   PID=$(echo "$DIR" | cut -d / -f 3)
   PROGNAME=$(ps -p "$PID" -o comm --no-headers)
-#    for SWAP in $(grep VmSwap "$DIR"/status 2>/dev/null | awk '{ print $2 }')
-  for SWAP in $(grep Swap "$DIR"/smaps 2>/dev/null | awk '{ print $2 }')
+  for SWAP in $(grep VmSwap "$DIR"/status 2>/dev/null | awk '{ print $2 }')
   do
     (( SUM=SUM+SWAP ))
   done
