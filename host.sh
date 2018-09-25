de6bba876002bc61642f0598c168eba6822abfc3
adamrocha <adamrocha13@gmail.com>
2018-09-25 07:43:11 -0700

    bash two way DNS

---
+++ host.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+host=$(host "$1")
+ip=$(echo "$host" | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}')
+
+echo
+printf "DNS resolves to...\\n\\n%s$host\\n\\n"
+printf "rDNS resolves to...\\n\\n"
+host "$ip"
+echo
