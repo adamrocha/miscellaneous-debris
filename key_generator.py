d67221e1468ee1db275b594852bc1975adecff8d
adamrocha <adamrocha13@gmail.com>
2018-09-29 19:12:56 -0700

    update naming convention

--- key_generator.py
+++ key_generator.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-# Generate a random key between 24 and 32 characters
+# Generate a random key defined by integer range
 
 from random import choice
 from random import randint
 import string
 
-characters = string.ascii_letters + string.punctuation + string.digits
+characters = string.ascii_letters + string.digits + string.punctuation
 password = "".join(choice(characters) for x in range(randint(24, 32)))
 print(password)
