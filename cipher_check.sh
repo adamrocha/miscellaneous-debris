231baf821257a036e055f855d5f41a2501c0c86e
adamrocha <adamrocha13@gmail.com>
2018-09-30 00:21:33 -0700

    syntax mods

--- cipher_check.sh
+++ cipher_check.sh
@@ -2,23 +2,23 @@
 # Check openssl ciphers against a URL.
 # OpenSSL requires the port number.
 
-SERVER=$1
-DELAY=1
+host=$1
+delay=1
 ciphers=$(openssl ciphers 'ALL:eNULL' | sed -e 's/:/ /g')
 
 echo Obtaining cipher list from "$(openssl version)".
 for cipher in $(printf '%s' "${ciphers[@]}")
 do
   echo -n Testing "$cipher"...
-  result=$(echo -n | openssl s_client -cipher "$cipher" -connect "$SERVER" 2>&1)
+  result=$(echo -n | openssl s_client -cipher "$cipher" -connect "$host" 2>&1)
   if [[ "$result" =~ ":error:" ]] ; then
     error=$(echo -n $result | cut -d':' -f6)
-    echo NO \("$error"\)
+    echo Failed \("$error"\)
   elif [[ "$result" = "Cipher is ${cipher}" || "$result" =~ "Cipher    :" ]] ; then
-    echo YES
+    echo "Success!"
   else
-    echo UNKNOWN RESPONSE
+    echo Unknown Response
     echo "$result"
   fi
-  sleep $DELAY
+  sleep $delay
 done
