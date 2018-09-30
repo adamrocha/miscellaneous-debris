10b40cc6d4e1f3210b11afac477dc1db3a3c1405
adamrocha <adamrocha13@gmail.com>
2018-09-29 20:34:47 -0700

    syntax mods

--- cipher_check.sh
+++ cipher_check.sh
@@ -7,7 +7,7 @@ DELAY=1
 ciphers=$(openssl ciphers 'ALL:eNULL' | sed -e 's/:/ /g')
 
 echo Obtaining cipher list from "$(openssl version)".
-for cipher in ${ciphers[@]}
+for cipher in $(printf '%s' "${ciphers[@]}")
 do
   echo -n Testing "$cipher"...
   result=$(echo -n | openssl s_client -cipher "$cipher" -connect "$SERVER" 2>&1)
