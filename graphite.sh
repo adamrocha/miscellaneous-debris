944b237fcc6188e82f6ba1bd7d30291dd9dc6542
adamrocha <adamrocha13@gmail.com>
2018-11-01 13:20:49 -0700

    testing graphite

---
+++ graphite.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Docker test script
+
+for i in {1..100}; do
+  if [ $i -eq 100 ]; then
+    break
+  else (echo "foo.bar $i `date +%s`" |nc localhost 2003)
+    sleep 10
+  fi
+done
