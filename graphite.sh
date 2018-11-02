f5e11307520b6161de2219bcc474d7d1239ede83
adamrocha <adamrocha13@gmail.com>
2018-11-02 08:33:16 -0700

    syntax correction

--- graphite.sh
+++ graphite.sh
@@ -4,7 +4,7 @@
 for i in {1..100}; do
   if [ $i -eq 100 ]; then
     break
-  else (echo "foo.bar $i `date +%s`" |nc localhost 2003)
+  else (echo "foo.bar $i $(date +%s)" |nc localhost 2003)
     sleep 10
   fi
 done
