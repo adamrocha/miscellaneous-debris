6d52c0c18b408b9710cf2bfbeb582a85d86635fc
adamrocha <adamrocha13@gmail.com>
2018-11-03 01:12:23 -0700

    graphite feed script

--- graphite.sh
+++ graphite.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 # Docker test script
 
-for i in {1..100}; do
-  if [ $i -eq 100 ]; then
-    break
-  else (echo "foo.bar $i `date +%s`" |nc localhost 2003)
-    sleep 10
-  fi
+#for i in {1..100}; do
+#  if [ $i -eq 100 ]; then
+#    break
+#  else (echo "foo.bar $i `date +%s`" |nc localhost 2003)
+#    sleep 10
+#  fi
+#done
+
+while true; do
+  echo "foo.bar $((1 + RANDOM % 100)) `date +%s`" |nc localhost 2003
+  sleep 10
 done
