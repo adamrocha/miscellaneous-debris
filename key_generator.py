140ec75d041f2da32c1227d9b533751861ee59df
adamrocha <adamrocha13@gmail.com>
2018-11-16 15:33:23 -0800

    syntax corrections

--- key_generator.py
+++ key_generator.py
@@ -6,5 +6,5 @@ from random import randint
 import string
 
 characters = string.ascii_letters + string.digits + string.punctuation
-password = "".join(choice(characters) for x in range(randint(24, 32)))
-print(password)
+key = "".join(choice(characters) for x in range(randint(24, 32)))
+print(key)
