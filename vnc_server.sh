a06481c2a495573192249488883e0dce7369cea0
adamrocha <adamrocha13@gmail.com>
2019-08-04 19:38:59 -0700

    copy pasta goof

--- vnc_server.sh
+++ vnc_server.sh
@@ -1,11 +1,10 @@
-1 #!/usr/bin/env bash
-2 # My VNC Server
-3
-4 /usr/local/bin/x11vnc \
-5  -auth /var/run/lightdm/root/:0 \
-6  -display :0 \
-7  -xkb \
-8  -forever \
-9  -shared \
-10  -rfbport 5900 \
-11 &>/dev/null & disown
+#!/usr/bin/env bash
+# My VNC Server
+/usr/local/bin/x11vnc \
+  -auth /var/run/lightdm/root/:0 \
+  -display :0 \
+  -xkb \
+  -forever \
+  -shared \
+  -rfbport 5900 \
+&>/dev/null & disown
