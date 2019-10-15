7416c910369917bbe240f06ad1ddf3d9e2f2b1cb
adamrocha <adamrocha13@gmail.com>
2019-10-14 20:48:20 -0700

    no dry run

--- purge_old_kernels.sh
+++ purge_old_kernels.sh
@@ -5,5 +5,5 @@ dpkg -l linux-{image,headers,modules}-* |\
 awk '{print $2}' |\
 grep -E '[0-9]+\.[0-9]+\.[0-9]+' |\
 grep -v "$(uname -r | cut -d- -f-2)" |\
-xargs apt purge -y --dry-run
+xargs apt purge -y
 update-grub2
