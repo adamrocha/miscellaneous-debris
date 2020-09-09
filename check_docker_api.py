a8da8e1ac36f1401552b9e46a50513155d276e54
adamrocha <adamrocha13@gmail.com>
2020-09-08 17:22:03 -0700

    clean and organize

--- check_docker_api.py
+++ check_docker_api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Query Docker replica API
-# Author: arocha 2020-08-26
+# Author: arocha 2020-09-08
 
 import argparse
 import json
@@ -12,20 +12,19 @@ parser.add_argument('-A', '--api', type = str, required = True)
 parser.add_argument('-U', '--username', type = str, required = True)
 parser.add_argument('-P', '--password', type = str, required = True)
 args = parser.parse_args()
-
 headers = {'Content-Type': 'application/json; charset=utf-8'}
 response = requests.get(args.api, headers=headers, auth=(args.username, args.password))
-objects = response.text.encode('utf8')
-replicaHealth = json.loads(objects)['replica_health']
 
 def replicaStatus():
+    objects = response.text.encode('utf8')
+    replicaHealth = json.loads(objects)['replica_health']
     for (k, v) in replicaHealth.items():
         v = str(v)
         if v == "OK":
-            print('HTTP: ' + str(response.status_code) + ' -- ' + 'Replicas Healthy: ' + str(replicaHealth))
+            print('Replicas Healthy: ' + str(replicaHealth))
             sys.exit(0)
         else:
-            print('HTTP: ' + str(response.status_code) + ' -- ' + 'Replicas Error: ' + str(replicaHealth))
+            print('Replicas Error: ' + str(replicaHealth))
             sys.exit(2)
 
 def main():
