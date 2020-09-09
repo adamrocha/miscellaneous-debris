3c81f80ad3da80fceeba071bae9b7a71e6f42df9
adamrocha <adamrocha13@gmail.com>
2020-09-08 19:16:51 -0700

    cleanup and include error control

--- check_docker_api.py
+++ check_docker_api.py
@@ -5,29 +5,43 @@
 import argparse
 import json
 import requests
-import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-A', '--api', type = str, required = True)
 parser.add_argument('-U', '--username', type = str, required = True)
 parser.add_argument('-P', '--password', type = str, required = True)
 args = parser.parse_args()
-headers = {'Content-Type': 'application/json; charset=utf-8'}
-response = requests.get(args.api, headers=headers, auth=(args.username, args.password))
+
+def authArgs():
+    headers = {'Content-Type': 'application/json; charset=utf-8'}
+    response = requests.get(args.api, headers=headers, auth=(args.username, args.password))
+    return response
+
+try:
+    def httpCode():
+        response = authArgs()
+        statusCode = response.status_code
+        if statusCode != 200:
+            print("HTTP Error: " + str(response.status_code))
+            exit(2)
+except Exception as e:
+    print(e)
 
 def replicaStatus():
+    response = authArgs()
     objects = response.text.encode('utf8')
     replicaHealth = json.loads(objects)['replica_health']
     for (k, v) in replicaHealth.items():
         v = str(v)
         if v == "OK":
             print('Replicas Healthy: ' + str(replicaHealth))
-            sys.exit(0)
+            exit(0)
         else:
             print('Replicas Error: ' + str(replicaHealth))
-            sys.exit(2)
+            exit(2)
 
 def main():
+    httpCode()
     replicaStatus()
 
 if __name__ == "__main__":
