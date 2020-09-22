9c04f2541ad07a0c818b9182cd56c8fbb5feceec
Adam Rocha <adam.rocha@arochseaosxl3.bigfish.lan>
2020-09-22 02:48:38 -0700

    enhancements

--- ssh_key_check.sh
+++ ssh_key_check.sh
@@ -1,7 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Test public key auth against a list. Some options listed.
+# -o PubkeyAuthentication=no
+# -o PasswordAuthentication=no
+# -o KbdInteractiveAuthentication=no
+# -o ChallengeResponseAuthentication=no
+# -o UserKnownHostsFile=/dev/null
+# -o StrictHostKeyChecking=no
+# -o ConnectTimeout=5
 
 while read -r line; do
-  ssh -o PasswordAuthentication=no -n "$line" hostname &>/dev/null
+  ssh \
+  -o PasswordAuthentication=no \
+  -o ChallengeResponseAuthentication=no \
+  -o UserKnownHostsFile=/dev/null \
+  -o StrictHostKeyChecking=no \
+  -o ConnectTimeout=5 \
+  -n "$line" &>/dev/null
   if [ $? -ne 0 ]; then
     echo "$line"
   fi
