8c761f58b60e56572e6741108bd83303480b4c68
Adam Rocha <adam.rocha@arochseaosxl3.bigfish.lan>
2020-09-26 18:55:36 -0700

    rebuild

--- graphite.py
+++ graphite.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 # Generate random numbers within range
 
-
 import random
-import subprocess
 import time
 
-while True:
+
+def randInt():
     r = random.randint(1, 300)
-#    t = time.time()
-    t = subprocess.getoutput(['date +%s'])
-    print("foo.bar " + str(r) + " " + str(t))
-    time.sleep(1)
+    t = time.strftime('%c')
+    return str(t) + " --> " + str(r)
+
+
+def main():
+    rand = randInt()
+    print(rand)
+
+if __name__ == "__main__":
+    main()
