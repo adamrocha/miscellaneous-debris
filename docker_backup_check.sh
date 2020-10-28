081431c64a8df3292243eef99ace6c86e0eb6147
Adam Rocha <adam.rocha@arochseaosxl3.bigfish.lan>
2020-10-28 13:08:56 -0700

    Add successful Docker backup conditional of +100M size

--- docker_backup_check.sh
+++ docker_backup_check.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
-# Report failed backup cycles occuring Saturdays ~ 06:30.
-# Author: arocha 2020-08-17
+# Report failed backup cycles scheduled Saturdays ~ 06:30.
+# Author: Adam Rocha 2020-08-17
+# Modified: Adam Rocha 2020-10-28
 
 currentTime=$(date +%s)
 
 # UCP backup repository
-UCPbackupFileCreated=$(stat -c %X "$(find /var/lib/dtr_backup/ucp/* -type f -exec ls -1rt {} + |tail -1)")
+UCPbackupFileCreated=$(stat -c %X "$(find /var/lib/dtr_backup/ucp/ -type f -size +100M -exec ls -1rt {} + |tail -1)")
 UCPdifference=$((currentTime - UCPbackupFileCreated))
 UCPsinceDate=$(date -d@"$UCPbackupFileCreated")
 
 # DTR backup repository
-DTRbackupFileCreated=$(stat -c %X "$(find /var/lib/dtr_backup/dtr/* -type f -exec ls -1rt {} + |tail -1)")
+DTRbackupFileCreated=$(stat -c %X "$(find /var/lib/dtr_backup/dtr/ -type f -size +100M -exec ls -1rt {} + |tail -1)")
 DTRdifference=$((currentTime - DTRbackupFileCreated))
 DTRsinceDate=$(date -d@"$DTRbackupFileCreated")
 
